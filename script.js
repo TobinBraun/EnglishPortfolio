6fbce02207d7e1f8f35b7748264b77be60c7feaa
TobinBraun <93841411+TobinBraun@users.noreply.github.com>
2024-10-04 11:31:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.json())
         .then(data => {
             const body = document.getElementById("body");
-
-            //const nameElement = document.createElement("p");
-            //nameElement.textContent = "Name: " + data.name;
-
-            //const ageElement = document.createElement("p");
-            //ageElement.textContent = "Age: " + data.age;
-
             const textDiv = document.createElement("p");
             textDiv.innerHTML = data.text;
-
-            // Append the elements to the "dataDisplay" div
-            dataDisplay.appendChild(textDiv);
+            body.appendChild(textDiv);
         })
         .catch(error => console.error("Error fetching JSON data:", error));
 });
